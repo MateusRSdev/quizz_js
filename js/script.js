35546919fd8dea0396ace1319e5405a56f422c2c
Mateus Ramos <112281721+MateusRSdev@users.noreply.github.com>
2023-04-13 11:47:22 -0300

    iniciando js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// declaracao de variaveis
+const question = document.querySelector("#question");
+const answersBox = document.querySelector("#answers-box");
+const quizzContainer = document.querySelector("#quizz-container");
+const scoreContainer = document.querySelector("#score-container");
+const letters = ["a","b","c","d"];
+let points = 0;
+let actualQuestion = 0;
+
+// perguntas
+
+
